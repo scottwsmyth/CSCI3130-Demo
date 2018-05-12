cf45ca09eb6552e18fc085fd1c635ad0094a8705
Scott <scott@TB6F6.WPA.Dal.Ca>
2018-05-12 16:16:46 -0300

    assignment 1

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button displayBtn = (Button) findViewById(R.id.displayBtn);
+        displayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText userText = (EditText) findViewById(R.id.stringText);
+                TextView result = (TextView) findViewById(R.id.resultText);
+
+                result.setText(userText.getText());
+
+            }
+        });
+
     }
 }
